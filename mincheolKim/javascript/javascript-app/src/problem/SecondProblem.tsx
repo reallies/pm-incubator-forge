16531a97fa8e595875cc9516a8cab1e299928e30
mincheol.kim <61931924+mch9@users.noreply.github.com>
2025-12-31 14:13:28 +0900

    feat: [김민철] for 루프 문제 풀이 [PMIF-x]

---
+++ mincheolKim/javascript/javascript-app/src/problem/SecondProblem.tsx
@@ -0,0 +1,23 @@
+// 3, 6, 9, 12, 15를 더한 결과를 출력해봅시다.
+export const SecondProblem = () => {
+    let loopResultArray = []
+    let summation = 0
+
+    for (let i = 3, j = 0; j <= 4; i += 3, j++) {
+        loopResultArray.push(i)
+        summation += loopResultArray[j]
+    }
+
+    return (
+        <div>
+            <h3>javascript 제어문 (for)</h3>
+
+            <pre>{
+                `
+loopResultArray = ${loopResultArray}
+summation = ${summation}
+                `
+            }</pre>
+        </div>
+    )
+}
